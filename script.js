463984527feb8473553c37f28d7382d23529ded0
GUILHERME <111024179+guileummi@users.noreply.github.com>
2026-03-10 16:06:56 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
 // Criar a estrutura de dados / Carregar dados salvos
+const nomeSalvo = localStorage.getItem('nomeJogador')
+
+if(nomeSalvo){
+    nomeJogador.textContent = nomeSalvo
+}
 
 const partidas = JSON.parse(localStorage.getItem('partidas')) || []
 
 // Pegar os elementos do HTML
+
+const nomeJogador = document.getElementById('nomeJogador')
+const btnAlterarNome = document.getElementById('btnAlterarNome')
 
 const inputJogo = document.getElementById('jogo')
 const selectResultado = document.getElementById('resultado')
@@ -16,11 +24,15 @@ const spanVitorias = document.getElementById('vitorias')
 const spanDerrotas = document.getElementById('derrotas')
 const spanDesempenho = document.getElementById('taxaD')
 
-const ctx = document.getElementById('grafico')
+const ctx = document.getElementById('graficoResultados')
+const ctxTempo = document.getElementById('graficoTempo')
+const filtroJogo = document.getElementById("filtroJogo")
 
 // Evento
 
 botao.addEventListener('click', adicionarPartida)
+filtroJogo.addEventListener("change", render)
+btnAlterarNome.addEventListener('click', alterarNome)
 
 // adicionar Partida
 
@@ -43,27 +55,44 @@ function adicionarPartida(){
     render()    
 }
 
-// salvar dados
+// salvar partidas
 
 function salvarDados(){
     localStorage.setItem('partidas', JSON.stringify(partidas))
+}
+
+// alterar nome
+
+function alterarNome(){
+    const novoNome = prompt("Digite seu nome")
+
+    if(novoNome){
+        nomeJogador.textContent = novoNome
+        localStorage.setItem('nomeJogador', novoNome)
+    }
+
 }
 
 // Mostrar as partidas
 
 function atualizarLista(){
     lista.innerHTML = ''
+    const jogoSelecionado = filtroJogo.value
 
     for(let i = 0; i < partidas.length; i++){
         const partida = partidas[i]
+
+        if(jogoSelecionado !== 'todos' && partida.jogo !== jogoSelecionado){
+            continue
+        }
 
         const li = document.createElement('li')
 
         const texto = document.createElement('span')
         texto.textContent = `🎮 ${partida.jogo} | 🏆 ${partida.resultado} | 📅 ${partida.data}`
 
-        const botaoDelete = document.createElement('buttom')
-        botaoDelete.textContent = "🗑️"
+        const botaoDelete = document.createElement('button')
+        botaoDelete.textContent = '🗑️'
 
         botaoDelete.onclick = function(){
             deletarPartida(i)
@@ -76,16 +105,25 @@ function atualizarLista(){
     }
 }
 
-// Atualiazar gráfico
+// deletar partida
+function deletarPartida(index){
 
-let grafico
+    partidas.splice(index, 1)
+
+    salvarDados()
+    render()
+}
+
+// Atualiazar gráfico resultado
+
+let graficoResultados
 
 function atualizarGrafico(vitorias, derrotas){
-    if(grafico){
-        grafico.destroy()
+    if(graficoResultados){
+        graficoResultados.destroy()
     }
 
-    grafico = new Chart(ctx, {
+    graficoResultados = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ['vitorias', 'derrotas'],
@@ -99,6 +137,69 @@ function atualizarGrafico(vitorias, derrotas){
             }]
         }
     })
+}
+
+// atualizar gráfico tempo
+
+let graficoDesempenho
+
+function atualizarGraficoTempo(){
+    const labels = []
+    const dados = []
+
+    let vitorias = 0
+
+    for(let i = 0; i < partidas.length; i++){
+        const partida = partidas[i]
+
+        if(partida.resultado === 'vitoria'){
+            vitorias++
+        }
+
+        labels.push('Partida' + (i + 1))
+        dados.push(vitorias)
+    }
+
+    if(graficoDesempenho){
+        graficoDesempenho.destroy()
+    }
+
+    graficoDesempenho = new Chart(ctxTempo, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Vitórias acumuladas',
+                data: dados,
+                borderColor: 'green',
+                fill: false
+            }]
+        }
+    })
+}
+
+// atualizar filtro
+function atualizarFiltro(){
+    const jogosUnicos = []
+
+    for(let i = 0; i < partidas.length; i++){
+        const jogo = partidas[i].jogo
+
+        if(!jogosUnicos.includes(jogo)){
+            jogosUnicos.push(jogo)
+        }
+    }
+
+    filtroJogo.innerHTML = '<option value="todos">Todos os jogos</option>'
+
+    for(let i = 0; i < jogosUnicos.length; i++){
+        const option = document.createElement('option')
+
+        option.value = jogosUnicos[i]
+        option.textContent = jogosUnicos[i]
+
+        filtroJogo.appendChild(option)
+    }
 }
 
 // Atualizar estatísticas
@@ -124,7 +225,7 @@ function atualizarEstatisticas(){
     }
 
     spanPartidas.textContent = partidas.length
-    spanPartidas.textContent = vitorias
+    spanVitorias.textContent = vitorias
     spanDerrotas.textContent = derrotas
     spanDesempenho.textContent = taxaDesempenho.toFixed(1) + '%'
 
@@ -132,8 +233,10 @@ function atualizarEstatisticas(){
 }
 
 function render(){
+    atualizarFiltro()
     atualizarLista()
     atualizarEstatisticas()
+    atualizarGraficoTempo()
 }
 
 // INICIALIZAÇÃO
